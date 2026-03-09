fb65aad2a5b86c58f6b72761501e2ddf096cbbca
abubakar khan <abubakaralam958@gmail.com>
2026-03-09 19:41:02 +0000

    Add files via upload

---
+++ LAB 2/LAB WORK/LAB_2 LabWork_2.cpp
@@ -0,0 +1,15 @@
+// Name: Abubakar Alam
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0680
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14;  // declare float variable pi
+    cout << "Value of pi = " << pi << endl;  // display the value
+    return 0;
+}
+
+
