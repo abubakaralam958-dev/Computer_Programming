3238b3cfcf7744c0ca37e8332786b9855c612169
abubakar khan <abubakaralam958@gmail.com>
2026-03-09 19:59:03 +0000

    Add files via upload

---
+++ LAB 1/LAB TASK/LAB_01 TASK_04.cpp
@@ -0,0 +1,17 @@
+// Name: Abubakar Alam
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0680
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;  // Declaring variable
+
+    cout << "a = " << a;  // Printing value of a
+
+    return 0;  // Program ends successfully
+}
+
