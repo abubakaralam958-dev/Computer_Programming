63f82ebf360b3c71ece425e769a749067999e2bf
abubakar khan <abubakaralam958@gmail.com>
2026-05-17 10:03:22 +0100

    Add files via upload

---
+++ Lab 01/Lab Work/LAB_01 LabWork_1.cpp
@@ -0,0 +1,17 @@
+// Name: Abubakar Alam
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0680
+// Section: B
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
